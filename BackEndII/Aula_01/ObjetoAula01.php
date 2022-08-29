d5b987ec7239319caf0e70e02f6950145fb8a52c
Rodrigo Oliveira <rodrigo.oliveirafln@gmail.com>
2022-08-29 12:59:17 -0300

    Pegando as atualizações no código do professor

--- BackEndII/Aula_01/ObjetoAula01.php
+++ BackEndII/Aula_01/ObjetoAula01.php
@@ -9,4 +9,25 @@ $objetoProduto = new Produto();
 var_dump $objetoProduto
 //edição feita no PHP Storm
 //TODO - fazer o teste do objeto
+  
+  require_once("Produto.php");
+
+//instanciar o objeto
+
+$objetoProduto = new Produto();
+
+$objetoProduto->setNome("Microfone");
+echo "Produto: " . $objetoProduto->getNome();
+
+//armazenar o código e o preço
+//resgatar o codigo e o preço
+
+/*$objetoProduto->nome = "Teclado";
+$objetoProduto->codigo = 458;
+$objetoProduto->preco = 54.89;
+echo $objetoProduto->codigo;
+echo " - " . $objetoProduto->nome;
+echo " - " . $objetoProduto->preco;*/
+
+//var_dump($objetoProduto);
   ?>
